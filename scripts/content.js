951ac2ba142aacb058939745c9fcf0771c4b21a2
Doug Smith <dougsmith2015@gmail.com>
2017-03-19 16:31:12 -0400

    Added dynamic changing of height to html navbar

--- scripts/content.js
+++ scripts/content.js
@@ -1,9 +1,8 @@
 //Stats for nav element
 var elemH = 15;
-var elemW = 75;
+var elemW = 120;
 //Stats for entire nav bar
 var barH = elemH;
-
 
 //Build-A-Bar
 bar = document.createElement('div');
@@ -23,28 +22,37 @@ bar.style['background-color']="white";
 chrome.storage.local.get("tabs", function(results){
   console.log(results);
   for (i in results['tabs']){
-    console.log( results['tabs'][i] );
     e = document.createElement('div');
     e.innerHTML = results['tabs'][i];
     e.style['white-space']="nowrap";
     e.style['overflow']="hidden";
+
+    //Border and padding to free border
+    e.style['border-style']="solid";
+    e.style['border-width']="1px";
+
     e.style.height=elemH+"px";
     e.style.width=elemW+"px";
-    
-    e.style.position='absolute';
-    e.style.left= elemW * i + "px";
-    e.style.top=0+"px";
+   
+    e.style.display="inline-block"; 
+    //e.style.position='absolute';
+    //e.style.left= elemW * i + "px";
+    //e.style.top=0+"px";
 
     bar.appendChild( e );
   }
 
 
+  barH = (elemH+2) * (results['tabs'].length /  ( document.body.offsetWidth  / elemW | 0 ) +1 |0 );
+  console.log( barH );
+
   //Move the rest of the document to make room for bar
-  document.body.style['margin-top'] = "20px";
+  bar.style.height = barH+'px';
+  document.body.style['margin-top'] = barH+"px";
   //Add the bar
   document.body.appendChild(bar);
-  console.log("CONTENT loaded");
 });
+
 
 
 
