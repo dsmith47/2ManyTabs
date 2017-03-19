38c40c6debfffa006bcfeabb11d0a4279ee55c1a
Doug Smith <dougsmith2015@gmail.com>
2017-03-19 18:36:58 -0400

    Added message sending for updating tabs BACKGROUND -> CONTENT

--- scripts/background.js
+++ scripts/background.js
@@ -1,9 +1,26 @@
 //Access our tabs and add our data to them
-chrome.tabs.query({} ,function(results){
-  titles=[]
-  for (i in results){
-    titles.push( results[i]['title'] );
-  }
-  //Store our titles list
-  chrome.storage.local.set({"tabs":titles});
-} );
+function storeTabs(){
+  chrome.tabs.query({} ,function(results){
+    titles=[]
+    for (i in results){
+      titles.push( results[i]['title'] );
+    }
+    //Store our titles list
+    chrome.storage.local.set({"tabs":titles});
+  } );
+}
+//Load a new tab list whenever tabs are updated
+chrome.tabs.onUpdated.addListener( function() {
+  storeTabs();
+  //Update all the nav bars
+  chrome.tabs.query({},function(tabs){
+    for (i in tabs){
+      chrome.tabs.sendMessage( tabs[i].id, "update", function(response){} );
+    }
+  });
+});
+
+
+
+//Run when background script first runs
+storeTabs();
--- scripts/content.js
+++ scripts/content.js
@@ -4,55 +4,71 @@ var elemW = 120;
 //Stats for entire nav bar
 var barH = elemH;
 
-//Build-A-Bar
-bar = document.createElement('div');
+var bar; //This will always be a <div> element
 
+function writeNavbar(){
 
-bar.style.display="block";
-bar.style.position="fixed";
-bar.style.left=0;
-bar.style.top=0;
+  //Build-A-Bar
+  bar = document.createElement('div');
+  bar.id = "2ManyTabsNavBar";
 
-bar.style.height=barH+"px";
-bar.style.width="100%";
+  bar.style.display="block";
+  bar.style.position="fixed";
+  bar.style.left=0;
+  bar.style.top=0;
 
-bar.style['background-color']="white";
+  bar.style.height=barH+"px";
+  bar.style.width="100%";
 
-//Load our current set of tabs
-chrome.storage.local.get("tabs", function(results){
-  console.log(results);
-  for (i in results['tabs']){
-    e = document.createElement('div');
-    e.innerHTML = results['tabs'][i];
-    e.style['white-space']="nowrap";
-    e.style['overflow']="hidden";
+  bar.style['background-color']="white";
 
-    //Border and padding to free border
-    e.style['border-style']="solid";
-    e.style['border-width']="1px";
+  //Load our current set of tabs
+  chrome.storage.local.get("tabs", function(results){
+    console.log(results);
+    for (i in results['tabs']){
+      e = document.createElement('div');
+      e.innerHTML = results['tabs'][i];
+      e.style['white-space']="nowrap";
+      e.style['overflow']="hidden";
 
-    e.style.height=elemH+"px";
-    e.style.width=elemW+"px";
+      //Border and padding to free border
+      e.style['border-style']="solid";
+      e.style['border-width']="1px";
+
+      e.style.height=elemH+"px";
+      e.style.width=elemW+"px";
    
-    e.style.display="inline-block"; 
-    //e.style.position='absolute';
-    //e.style.left= elemW * i + "px";
-    //e.style.top=0+"px";
+      e.style.display="inline-block"; 
+      //e.style.position='absolute';
+      //e.style.left= elemW * i + "px";
+      //e.style.top=0+"px";
 
-    bar.appendChild( e );
-  }
+      bar.appendChild( e );
+    }
 
 
-  barH = (elemH+2) * (results['tabs'].length /  ( document.body.offsetWidth  / elemW | 0 ) +1 |0 );
-  console.log( barH );
+    barH = (elemH+2) * (results['tabs'].length /  ( document.body.offsetWidth  / elemW | 0 ) +1 |0 );
+    console.log( barH );
 
-  //Move the rest of the document to make room for bar
-  bar.style.height = barH+'px';
+    //Move the rest of the document to make room for bar
+    bar.style.height = barH+'px';
   document.body.style['margin-top'] = barH+"px";
-  //Add the bar
-  document.body.appendChild(bar);
+    //Add the bar
+    document.body.appendChild(bar);
+  });
+
+}
+
+//Update nav bar when tabs update
+chrome.extension.onMessage.addListener(function(msg,sender, sendResponse){
+  console.log("UPDATE");
+  //remove old navbar
+  bar.parentNode.removeChild(bar);
+  document.body.style['margin-top']="0px";
+  //Write a new navbar
+  writeNavbar();
 });
 
 
-
-
+//Run at startup to build initial nav bar
+writeNavbar();
