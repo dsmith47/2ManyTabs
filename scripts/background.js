820a15efee059f6f0b1e384482b886554d2eeadf
Doug Smith <dougsmith2015@gmail.com>
2017-03-18 22:16:38 -0400

    Added simple display of current tabs

--- scripts/background.js
+++ scripts/background.js
@@ -1,2 +1,9 @@
-//Background scripts can't use console
-//alert("Loaded from BACKGROUND");
+//Access our tabs and add our data to them
+chrome.tabs.query({} ,function(results){
+  titles=[]
+  for (i in results){
+    titles.push( results[i]['title'] );
+  }
+  //Store our titles list
+  chrome.storage.local.set({"tabs":titles});
+} );
--- scripts/content.js
+++ scripts/content.js
@@ -1,22 +1,50 @@
+//Stats for nav element
+var elemH = 15;
+var elemW = 75;
+//Stats for entire nav bar
+var barH = elemH;
+
+
 //Build-A-Bar
 bar = document.createElement('div');
-bar.innerHTML = 'THIS IS A DIV';
+
 
 bar.style.display="block";
 bar.style.position="fixed";
 bar.style.left=0;
 bar.style.top=0;
 
-bar.style.height="20px";
+bar.style.height=barH+"px";
 bar.style.width="100%";
 
-//Move the rest of the document to make room for bar
-document.body.style['margin-top'] = "20px";
-//(int)document.body.style['margin-top'] + 10 + "px";
+bar.style['background-color']="white";
 
-//Add the bar
-document.body.appendChild(bar);
+//Load our current set of tabs
+chrome.storage.local.get("tabs", function(results){
+  console.log(results);
+  for (i in results['tabs']){
+    console.log( results['tabs'][i] );
+    e = document.createElement('div');
+    e.innerHTML = results['tabs'][i];
+    e.style['white-space']="nowrap";
+    e.style['overflow']="hidden";
+    e.style.height=elemH+"px";
+    e.style.width=elemW+"px";
+    
+    e.style.position='absolute';
+    e.style.left= elemW * i + "px";
+    e.style.top=0+"px";
 
-console.log("CONTENT loaded");
+    bar.appendChild( e );
+  }
+
+
+  //Move the rest of the document to make room for bar
+  document.body.style['margin-top'] = "20px";
+  //Add the bar
+  document.body.appendChild(bar);
+  console.log("CONTENT loaded");
+});
+
 
 
